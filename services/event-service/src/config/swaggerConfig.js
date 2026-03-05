d8ab97fdb47bc933116eae25d21decce2719258b
chamalkaMarasinghe <chamalkamarasinghe11@gmail.com>
2026-03-05 11:51:37 +0530

    event service api documentation

--- services/event-service/src/config/swaggerConfig.js
+++ services/event-service/src/config/swaggerConfig.js
@@ -1,14 +1,5 @@
 const swaggerJSDoc = require("swagger-jsdoc");
-const authDocs = require("../docs/authDocs");
-const userDocs = require("../docs/userDocs");
-const serviceProviderDocs = require("../docs/serviceProvider");
-const threadDocs = require("../docs/threadDocs");
-const offerDocs = require("../docs/offer");
-const jobPostDocs = require('../docs/jobPostDocs');
 const currentEnvironment = require('./environmentConfig');
-const orderDocs = require("../docs/orderDocs");
-const paymentDocs = require("../docs/paymentDocs");
-const reviewDocs = require("../docs/reviewDocs");
 const eventDocs = require("../docs/eventDocs");
 const BASE_URL = currentEnvironment.BASE_URL;
 const API_VERSION = currentEnvironment.API_VERSION;
@@ -17,21 +8,12 @@ const options = {
     definition: {
         openapi: "3.0.0",
         info: {
-            title: "KidsPlan Project API",
+            title: "KidsPlan Event Service",
             version: "1.0.0",
-            description: "API documentation of the KidsPlan project",
+            description: "API documentation of the KidsPlan API Event service",
         },
         paths: {
-            ...authDocs,
-            ...serviceProviderDocs,
-            ...userDocs,
             ...eventDocs,
-            ...reviewDocs,
-            ...threadDocs,
-            // ...offerDocs,
-            // ...jobPostDocs,
-            // ...orderDocs,
-            // ...paymentDocs
         },
         servers: [{ url: `${BASE_URL}/api/v${API_VERSION}` }],
         components: {
--- services/event-service/src/routes/event.js
+++ services/event-service/src/routes/event.js
@@ -11,23 +11,10 @@ const { createNewEvent ,getAllEvents, getEventById, bookEventByUser, getAllAtten
 
 router.post('/', authenticateServiceProvider(), authorizeRoles([`${roles.USER}`]), createNewEvent);
 
-// NOTE: Get all the events based on query parameters 
 router.get('/all', authenticateUser(), authorizeRoles([`${roles.USER}`]), getAllEvents);
 
-router.get('/all/service-pro', authenticateServiceProvider(), authorizeRoles([`${roles.SERVICE_PRO}`, `${roles.ADMIN}`]), getAllEvents);
-
-router.get('/all/attendees/:id', authenticateServiceProvider(), authorizeRoles([`${roles.SERVICE_PRO}`]), getAllAttendeesOfSingleEvent);
-
-router.get('/all/attendees', authenticateServiceProvider(), authorizeRoles([`${roles.SERVICE_PRO}`]),getAllAttendeesOfAllEvents);
-
-// NOTE get event by id 
 router.get('/user/:id', authenticateUser(), authorizeRoles([`${roles.USER}`]), validate, getEventById);
 
-router.get('/service-pro/:id', authenticateServiceProvider(), authorizeRoles([`${roles.SERVICE_PRO}`, `${roles.ADMIN}`]), getEventById);
-
-router.get('/service-pro/:id',authenticateServiceProvider(), authorizeRoles([`${roles.SERVICE_PRO}`, `${roles.ADMIN}`]), getEventById);
-
-// NOTE: BOOK a event by the users 
 router.post('/book-event', authenticateUser(), authorizeRoles([`${roles.USER}`]), bookEventByUser);
 
 router.get('/organizer/:id', getEventOrganizer);
