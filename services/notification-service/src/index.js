301eb74f4f50074df75c511600d10fa27e56cdbd
chamalkaMarasinghe <108983037+chamalkaMarasinghe@users.noreply.github.com>
2026-03-09 12:32:19 +0530

    Update index.js

--- services/notification-service/src/index.js
+++ services/notification-service/src/index.js
@@ -1,4 +1,4 @@
-const express = require("express");//..
+const express = require("express");//...
 const cors = require("cors");
 const amqp = require("amqplib");
 require("dotenv").config({ path: "../.env" });
