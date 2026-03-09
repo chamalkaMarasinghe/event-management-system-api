5e0bc9e9a1b697751f9e4355cd8e7d211e2dd850
chamalkaMarasinghe <chamalkamarasinghe11@gmail.com>
2026-03-09 21:32:26 +0530

    start consuming method retreis

--- services/notification-service/src/index.js
+++ services/notification-service/src/index.js
@@ -26,30 +26,51 @@ app.get("/", async (req, res, next) => {
 // NOTE: consuming queue messages - listening for user_booked-event
 async function startConsuming() {
   try {
-    connection = await amqp.connect(`amqp://${currentEnvironment.RABBIT_MQ_URL}`);
-    channel = await connection.createChannel();
+    console.log(`amqp://${currentEnvironment.RABBIT_MQ_URL}`);
+    let retry = 5;
+    let connection = null, channel = null;
 
-    await channel.assertQueue("user_booked_event");
-    console.log("Notification Service is listening to messages");
+    while(retry){
+      try {
+        connection = await amqp.connect(`amqp://${currentEnvironment.RABBIT_MQ_URL}`);            
+        channel = await connection?.createChannel();
+        await channel.assertQueue("user_booked_event");
+        console.log("Notification Service is listening to messages");
+        return;
+      } catch (error) {
+        console.error("Retrying RabbitMQ Connection Error : " , error.message);
+        retry --;
+        console.error("Retrying again: " , retry);
+        await new Promise(res => setTimeout(res, 3000));
+      }
+    }
+    
+    // connection = await amqp.connect(`amqp://${currentEnvironment.RABBIT_MQ_URL}`);
+    // channel = await connection.createChannel();
 
-    channel.consume("user_booked_event", (msg) => {      
-      const data = JSON.parse(msg.content.toString());
-      // console.log("Notification: NEW DATA: ", data);
+    // await channel.assertQueue("user_booked_event");
+    // console.log("Notification Service is listening to messages");
 
-      // NOTE: sending confirmation email to the user
+    if(channel){
+      channel?.consume("user_booked_event", (msg) => {      
+        const data = JSON.parse(msg.content.toString());
+        // console.log("Notification: NEW DATA: ", data);
 
-      sendNotifications({
-        isSendingEmail: true,
-        emailOptions: {
-          email: data?.email,
-          subject: `The event was successfully booked.`,
-          topic: 'Your event booking is confirmed',
-          content: `Please note that, You have booked ${data?.event?.name}.`,
-        }
+        // NOTE: sending confirmation email to the user
+
+        sendNotifications({
+          isSendingEmail: true,
+          emailOptions: {
+            email: data?.email,
+            subject: `The event was successfully booked.`,
+            topic: 'Your event booking is confirmed',
+            content: `Please note that, You have booked ${data?.event?.name}.`,
+          }
+        });
+
+        channel.ack(msg);
       });
-
-      channel.ack(msg);
-    });
+    }
 
   } catch (error) {
     console.error("RabbitMQ Connection Error : " , error.message);
